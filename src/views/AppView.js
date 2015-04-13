8e550b5977de1fb861e0721408bd9d36afc7debc
RussHR <russ.rinzler@gmail.com>
2015-04-13 14:03:29 -0700

    making initial commit before I kill everything

---
+++ src/views/AppView.js
@@ -0,0 +1,28 @@
+/*** AppView ***/
+
+define(function(require, exports, module) {
+  var View = require('famous/core/View');
+  var Surface = require('famous/core/Surface');
+  var Transform = require('famous/core/Transform');
+  var StateModifier = require('famous/modifiers/StateModifier');
+
+  // import the SlideshowView class
+  var SlideshowView = require('views/SlideshowView');
+
+  function AppView() {
+    View.apply(this, arguments);
+
+    // create a new instance of slideshow view
+    var slideshowView = new SlideshowView();
+
+    // add the instance to app view
+    this.add(slideshowView);
+  }
+
+  AppView.prototype = Object.create(View.prototype);
+  AppView.prototype.constructor = AppView;
+
+  AppView.DEFAULT_OPTIONS = {};
+
+  module.exports = AppView;
+});
