0e6e4c3003bf7936e6818e544155820c53807f02
RussHR <russ.rinzler@gmail.com>
2015-04-13 18:06:56 -0700

    supposedly implement lightbox, gonna start with image fetching

--- src/views/SlideshowView.js
+++ src/views/SlideshowView.js
@@ -1,25 +1,38 @@
 /*** SlideshowView ***/
 
 define(function(require, exports, module) {
-    var View = require('famous/core/View');
-    var Surface = require('famous/core/Surface');
-    var Transform = require('famous/core/Transform');
-    var StateModifier = require('famous/modifiers/StateModifier');
+  var View = require('famous/core/View');
+  var Surface = require('famous/core/Surface');
+  var Transform = require('famous/core/Transform');
+  var StateModifier = require('famous/modifiers/StateModifier');
+  var Lightbox = require('famous/views/Lightbox');
 
-    var SlideView = require('views/SlideView');
+  function SlideshowView() {
+    View.apply(this, arguments);
 
-    function SlideshowView() {
-      View.apply(this, arguments);
+    this.rootModifier = new StateModifier({
+      size: this.options.size,
+      origin: [0.5, 0],
+      align: [0.5, 0]
+    });
 
-      var slideView = new SlideView();
+    this.mainNode = this.add(this.rootModifier);
 
-      this.add(slideView);
-    }
+    _createLightbox.call(this);
+  }
 
-    SlideshowView.prototype = Object.create(View.prototype);
-    SlideshowView.prototype.constructor = SlideshowView;
+  SlideshowView.prototype = Object.create(View.prototype);
+  SlideshowView.prototype.constructor = SlideshowView;
 
-    SlideshowView.DEFAULT_OPTIONS = {};
+  SlideshowView.DEFAULT_OPTIONS = {
+    size: [450, 500],
+    lightboxOpts: {}
+  };
 
-    module.exports = SlideshowView;
+  function _createLightbox() {
+    this.lightbox = new Lightbox(this.options.lightboxOpts);
+    this.mainNode.add(this.lightbox);
+  }
+
+  module.exports = SlideshowView;
 });
